2910ac02dc905c7049e389f8333372eaae0aeab7
Santosh <mailsantoshsapkota@gmail.com>
2018-10-13 23:54:07 +1100

    Test class create for service charge

---
+++ src/test/ServiceChargeTest.java
@@ -0,0 +1,32 @@
+package test;
+import hotel.entities.ServiceType;
+import hotel.service.RecordServiceCTL;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class ServiceChargeTest {
+	
+	    public ServiceChargeTest() {
+	    }
+	    
+	    @BeforeClass
+	    public static void setUpClass() {
+	    }
+	    
+	    @AfterClass
+	    public static void tearDownClass() {
+	    }
+	    
+	    @Before
+	    public void setUp() {
+	    }
+	    
+	    @After
+	    public void tearDown() {
+	    }
+
+	   
